99753e01dcc436aa83e0acb27802ec348db84306
ibraheem Hasan <rules656@gmail.com>
2019-12-11 16:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ class Scooter{
     this.year=year;
     this.model=model;
     this.color=color;}}
-    
+class Driver{
+constructor(name,age,experience){
+  this.name=name;
+  this.age=age;
+  this.experience=experience;}}
+  class PickupLocation{
+    constructor(address,city){
+      this.address=address;
+      this.city=city;}}
